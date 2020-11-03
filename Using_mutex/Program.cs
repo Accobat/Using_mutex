4558d0aef173f180568b3d5aea7f241bf9471d05
Алексей Подругин <podrugin@hotmail.com>
2020-11-03 18:05:42 +0300

    Добавьте файлы проекта.

---
+++ Using_mutex/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+namespace Using_mutex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mutex oneMutex = null;
+            const String MutexName = "RUNMEONLYONCE";
+            try
+            {
+                oneMutex = Mutex.OpenExisting(MutexName);
+            }
+            catch (WaitHandleCannotBeOpenedException)
+            {
+                oneMutex = new Mutex(true, MutexName);
+
+            }
+    
+       
+                    oneMutex.Close();
+                    return;
+      
+            
+            Console.WriteLine("Our Application");
+            Console.Read();
+}
+    }
+}
+    
+
